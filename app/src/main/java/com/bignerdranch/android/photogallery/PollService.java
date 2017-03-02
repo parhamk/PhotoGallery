c9a77db2ae7e8739a0dc6a4d883ce56ae199cf8d
parham <parham.contact@gmail.com>
2017-03-02 15:31:55 +0330

    Initial commit

---
+++ app/src/main/java/com/bignerdranch/android/photogallery/PollService.java
@@ -0,0 +1,67 @@
+package com.bignerdranch.android.photogallery;
+
+import android.app.IntentService;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.util.Log;
+
+import java.util.List;
+
+/**
+ * Created by parham on 2/28/2017 AD.
+ */
+
+public class PollService extends IntentService {
+    private static final String TAG = "PollService";
+
+    public static Intent newIntent(Context context) {
+        return new Intent(context, PollService.class);
+    }
+
+    public PollService() {
+        super(TAG);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+        if (!isNetworkAvailableAndConnected()) {
+            return;
+        }
+
+        String query = QueryPreferences.getStoredQuery(this);
+        String lastResultId = QueryPreferences.getLastResultId(this);
+
+        List<GalleryItem> items;
+
+        if (query == null) {
+            items = new FlickrFetchr().fetchRecentPhotos();
+        } else {
+            items = new FlickrFetchr().searchPhotos(query);
+        }
+
+        if (items.size() == 0) {
+            return;
+        }
+
+        String resultId = items.get(0).getId();
+        if (resultId.equals(lastResultId)) {
+            Log.i(TAG, "Got an old reslut: " + resultId);
+        } else {
+            Log.i(TAG, "Got a new result: " + resultId);
+        }
+
+        QueryPreferences.seLastResultId(this, resultId);
+    }
+
+    public boolean isNetworkAvailableAndConnected() {
+
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(CONNECTIVITY_SERVICE);
+
+        boolean isNetworkAvailable = cm.getActiveNetworkInfo() != null;
+        boolean isNetworkConnected = isNetworkAvailable && cm.getActiveNetworkInfo().isConnected();
+
+        return isNetworkConnected;
+    }
+}
